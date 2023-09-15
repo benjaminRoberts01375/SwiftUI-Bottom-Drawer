be9e75fdb41d68e311a646dc3820b3ba4a7ed1f8
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-09-14 22:26:59 -0400

    Initial commit

---
+++ Tests/Bottom DrawerTests/Bottom_DrawerTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import Bottom_Drawer
+
+final class Bottom_DrawerTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
