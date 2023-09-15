e14ec6c9e3499b8b20bc13bd51bf414335e332b5
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-09-15 12:19:44 -0400

    Setup preview

--- Sources/Bottom Drawer/Bottom_Drawer.swift
+++ Sources/Bottom Drawer/Bottom_Drawer.swift
@@ -1,2 +1,35 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+// Created on 9/14/23 by Ben Roberts
+// Created for the infinite grid library
+//
+// Swift 5.0
+//
+
+import SwiftUI
+
+@available(iOS 16.0, macOS 13.0, *)
+public struct BottomDrawer: View {
+    private let cornerRadius = 20
+    
+    public init () { }
+    
+    public var body: some View {
+        ZStack {
+            EmptyView()
+        }
+    }
+}
+
+@available(iOS 16.0, macOS 13.0, *)
+#Preview {
+    var colors: [Color] = [.blue, .yellow, .purple, .pink, .red, .brown, .cyan, .gray, .green]
+    let color1 = colors.randomElement()!
+    colors.remove(at: colors.firstIndex(of: color1)!)
+    let color2 = colors.randomElement()!
+ 
+    return ZStack {
+        LinearGradient(gradient: Gradient(colors: [color1, color2]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            .ignoresSafeArea()
+        
+        BottomDrawer()
+    }
+}
