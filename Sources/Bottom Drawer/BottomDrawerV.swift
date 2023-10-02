41493806d1657150172c9c13778306153873f37d
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-10-02 17:06:32 -0400

    Fix padding for some devices
    Only use padding for devices that don't have leading or trailing safe areas.

--- Sources/Bottom Drawer/BottomDrawerV.swift
+++ Sources/Bottom Drawer/BottomDrawerV.swift
@@ -105,8 +105,8 @@ public struct BottomDrawer: View {
                     .scrollDisabled(!controller.scrollable)
                 }
                 .drawerLayer()
-                .padding(.leading, abs(geo.safeAreaInsets.leading - geo.safeAreaInsets.bottom))
-                .padding(.trailing, abs(geo.safeAreaInsets.trailing - geo.safeAreaInsets.bottom))
+                .padding(.leading, controller.isShortCard && geo.safeAreaInsets.leading < geo.safeAreaInsets.bottom ? abs(geo.safeAreaInsets.leading - geo.safeAreaInsets.bottom) : 0)
+                .padding(.trailing, controller.isShortCard && geo.safeAreaInsets.trailing < geo.safeAreaInsets.trailing ? abs(geo.safeAreaInsets.trailing - geo.safeAreaInsets.bottom): 0)
                 .frame(
                     width: controller.isShortCard ? controller.shortCardSize : geo.size.width,
                     height: controller.height
